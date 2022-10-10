9a3681e05c4a517b52f84d1a71e11f03ac49e7d8
jquevedo82 <ing.cheo@gmail.com>
2022-10-10 17:01:22 -0300

    feat: add order by icon and funcionality

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -7,9 +7,31 @@ use App\Models\Post;
 class ShowPosts extends Component
 {
     public $search;
+    public $sort='id';
+    public $direction='desc';
+
     public function render()
     {   $posts = Post::where('title','LIKE','%' . $this->search . '%')
-            ->orWhere('content','LIKE','%' . $this->search . '%')->get();
+            ->orWhere('content','LIKE','%' . $this->search . '%')
+            ->orderBy($this->sort, $this->direction)
+            ->get();
         return view('livewire.show-posts',compact('posts'));
+    }
+    public function order($sort){
+        if ($this->sort == $sort) {
+            if ($this->direction == 'desc') {
+                $this->direction= 'asc';
+                # code...
+            } else {
+                $this->direction= 'desc';
+                # code...
+            }
+
+        } else {
+            $this->sort=$sort;
+            $this->direction= 'asc';
+        }
+
+
     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -9,53 +9,81 @@
         <!-- component -->
         <x-table>
             <div class="px-6 py-4">
-                <x-jet-input class="w-full" placeholder="Escriba que quiere buscar" type="text" wire:model="search"></x-jet-input>
+                <x-jet-input class="w-full" placeholder="Escriba que quiere buscar" type="text" wire:model="search">
+                </x-jet-input>
             </div>
             @if ($posts->count())
-            <table class="min-w-full divide-y divide-gray-200">
-                <thead>
-                    <tr>
-                        <th
-                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                            ID
-                        </th>
-                        <th
-                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                            Title
-                        </th>
-                        <th
-                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                            Content
-                        </th>
-                        <th scope="col" class="relative px-6 py-3">
-                            <span class="sr-only">Edit</span>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($posts as $post)
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead>
                         <tr>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <p class="text-gray-900 whitespace-no-wrap">{{ $post->id }}</p>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <span
-                                    class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                                    {{ $post->title }}
-                                </span>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <p class="text-gray-900 whitespace-no-wrap">
-                                    {{ $post->content }}
-                                </p>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                            </td>
+                            <th scope="col" wire:click="order('id')"
+                                class="cursor-pointer px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                ID
+                                @if ($sort == 'id')
+                                    @if ($direction == 'asc')
+                                        <i class="fa-solid fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    @else
+                                        <i class="fa-solid fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    @endif
+                                @else
+                                    <i class="fa-solid fa-sort float-right mt-1"></i>
+                                @endif
+                            </th>
+                            <th scope="col" wire:click="order('title')"
+                                class="cursor-pointer px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                Title
+                                @if ($sort == 'title')
+                                    @if ($direction == 'asc')
+                                        <i class="fa-solid fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    @else
+                                        <i class="fa-solid fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    @endif
+                                @else
+                                    <i class="fa-solid fa-sort float-right mt-1"></i>
+                                @endif
+                            </th>
+                            <th scope="col" wire:click="order('content')"
+                                class="cursor-pointer px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                Content
+                                @if ($sort == 'content')
+                                    @if ($direction == 'asc')
+                                        <i class="fa-solid fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    @else
+                                        <i class="fa-solid fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    @endif
+                                @else
+                                    <i class="fa-solid fa-sort float-right mt-1"></i>
+                                @endif
+                            </th>
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                            </th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{{ $post->id }}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <span
+                                        class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                        {{ $post->title }}
+                                    </span>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        {{ $post->content }}
+                                    </p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             @else
                 <div class="px-6 py-4">
                     No existe ningun registro que mostrar
